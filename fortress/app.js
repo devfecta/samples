2f61a4dbb68cc24016fb3b1719ce78e901077872
Kevin Kelm <devfecta@gmail.com>
2020-01-02 01:43:38 -0600

    Just need to format the arrays for display

--- fortress/app.js
+++ fortress/app.js
@@ -26,23 +26,75 @@ app.post('/result', (request, response) => {
 
         let galaxies = [];
 
-        let readData = request.files.csvDocument.data.toString();
+        let readData = request.files.csvDocument.data.toString().trim();
 
-        console.log(readData);
+        // console.log(readData);
 
 
         csv.parse(readData, {columns: true}, function(err, data) {
+            /*
+            A = 1  B = 2  C = 3  D = 4  E = 5  F = 6  G = 7  H = 8
+            I = 9  J = 10  K = 11  L = 12  M = 13  N = 14  O = 15
+            P = 16  Q = 17  R = 18  S = 19  T = 20  U = 21  V = 22
+            W = 23  X = 24  Y = 25  Z = 26
+            */
 
             galaxies = JSON.parse(JSON.stringify(data, null, 2));
 
-            console.log(galaxies[0].title);
+            // console.log(galaxies);
 
-            response.write(galaxies[0].title);
+            response.writeHead(200, {'Content-Type': 'text/html','Content-Length':galaxies.length});
+
+            let oddFileNames = galaxies
+            .map((g) => {
+                if (g.file_name.charCodeAt(0) % 2) {
+                    // console.log(g.file_name.charCodeAt(0) + ' - ' + g.file_name.charAt(0));
+                    return g.file_name;
+                }
+            })
+            .filter(name => name != undefined);
+            console.log(oddFileNames);
+
+            let uniqueTitles = galaxies
+            .map(galaxy => galaxy.title)
+            .filter((name, index, galaxyNames) => 
+                {
+                    if (galaxyNames.indexOf(name) == index) {
+                        return name;
+                    }
+                }
+            );
+            console.log(uniqueTitles);
+
+            let nestedGalaxies = galaxies
+            .map(galaxy => { return {'id': galaxy.file_id, 'name': galaxy.file_name} })
+            .map(parents => 
+                {
+                    //console.log(parent.id + '-' + galaxies[index].parent_id);
+                    let children = galaxies.filter(galaxy => {
+                        if (galaxy.parent_id == parents.id) {
+                            //console.log(galaxy.parent_id + '=' + parents.id);
+                            //console.log(parents.name + ">" + galaxy.title);
+                            return galaxy.title;
+                        }
+                    })
+                    .map(g => g.title);
+
+                    return {'parent': parents.name, 'children': children};
+                }
+            );
+            
+
+            console.log(nestedGalaxies);
+
+            response.write('test');
+
+            
             response.end();
             
         });
 
-        console.log(galaxies);
+        //console.log(galaxies);
 
         /*
         csvJson().fromStream(request.files.csvDocument.data)
